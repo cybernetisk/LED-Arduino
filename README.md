d6b01d4eb946e3b321a5340a55c5e8118ba7835d
Adrian Helle <adrianhelle@gmail.com>
2016-07-11 18:58:28 +0200

    Created a branch for the new version

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 
 Dette repoet inneholder koden som ligger på arduinoen som ligger bak baren
 i Escape.
+
+## Changelog:
+
+* Progget for interfacen (en fysisk modul med kanpper og sliders)
+* Lagt til forskjellige modes
+	* Egen mode for å sette hver farge (3 knapper og et potmeter)
+	* Egen mode for å fade gjennom blå-rød
+* Lagt til en knapp for av og på
--- escape-lys.ino
+++ escape-lys.ino
@@ -2,12 +2,21 @@
  * This code fades through the BLUE and RED spectre of a RGB LED-strip
  * See: https://github.com/cybrairai/LED-Arduino/
  * 
- * The pin used for RED is 6 and 5 for BLUE, which is defined bellow
- * 
  * Author: Adrian Helle (adriah)
  */
 #define RED 6
+#define GREEN 3
 #define BLUE 5
+#define BUTTON_MODE 2
+#define BUTTON_MODE2 4
+#define BUTTON_R 7
+#define BUTTON_G 8
+#define BUTTON_B 10
+#define BUTTON_OFF_ON 9
+//#define LATCH_PIN 11
+//#define CLOCK_PIN 12
+//#define DATA_PIN 13
+#define POT_METER A0
 
 //The current fade value
 int valRed;
@@ -17,16 +26,84 @@ int valBlue;
 bool downR;
 bool downB;
 
+bool fade;
+bool setR;
+bool setG;
+bool setB;
+bool off;
+
 void setup() {
   pinMode(RED, OUTPUT);
   pinMode(BLUE, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(POT_METER, INPUT);
+  pinMode(BUTTON_MODE, INPUT_PULLUP);
+  pinMode(BUTTON_MODE2, INPUT_PULLUP);
+  pinMode(BUTTON_R, INPUT_PULLUP);
+  pinMode(BUTTON_G, INPUT_PULLUP);
+  pinMode(BUTTON_B, INPUT_PULLUP);
+  pinMode(BUTTON_OFF_ON, INPUT_PULLUP);
   valRed = 0;
   valBlue = 125;
   downR = false;
   downB = true;
+  Serial.begin(9600);
+  fade = true;
+  setR = false;
+  setG = false;
+  setB = false;  
 }
 
 void loop() {
+  if (digitalRead(BUTTON_MODE) == LOW) {
+    fade = true;
+    setR = false;
+    setG = false;
+    setB = false;  
+    Serial.println("MODE");
+  } else if (digitalRead(BUTTON_R) == LOW) {
+    fade = false;
+    setR = true;
+    setG = false;
+    setB = false;
+    Serial.println("R");
+  } else if (digitalRead(BUTTON_G) == LOW) {
+    fade = false;
+    setR = false;
+    setG = true;
+    setB = false;
+    Serial.println("G");
+  } else if (digitalRead(BUTTON_B) == LOW) {
+    fade = false;
+    setR = false;
+    setG = false;
+    setB = true;
+    Serial.println("B");
+  } else if (digitalRead(BUTTON_OFF_ON) == LOW){
+    fade = false;
+    setR = false;
+    setG = false;
+    setB = false;
+    Serial.println("OFF");
+
+    analogWrite(RED, 0);
+    analogWrite(GREEN, 0);
+    analogWrite(BLUE, 0);
+  }
+
+  if (fade) {
+    fader();
+  } else if (setR) {
+    analogWrite(RED, analogRead(POT_METER)*1023/255);
+  } else if (setG) {
+    analogWrite(GREEN, analogRead(POT_METER)*1023/255);
+  } else if (setB) {
+    analogWrite(BLUE, analogRead(POT_METER)*1023/255);    
+  }
+  Serial.println(analogRead(POT_METER));
+}
+
+void fader() {
   if (valRed == 250) {
     downR = true;
   } else if (valRed == 5){
@@ -54,3 +131,4 @@ void loop() {
   
   delay(60);
 }
+
